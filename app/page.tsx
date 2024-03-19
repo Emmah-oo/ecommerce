d3a1abb144a8951e56a1baa71d9841ee921517f7
Emmah-oo <emmaochonogor715@gmail.com>
2024-03-19 14:19:05 +0100

    finished desktop version

--- app/page.tsx
+++ app/page.tsx
@@ -19,21 +19,60 @@ export default function Home() {
     );
   };
   return (
-    <main>
+    <main className="min-h-[100vh]">
       <Header />
-      <div className="flex flex-col lg:flex-row lg:items-center lg:justify-center">
-        <div className="mt-5 relative">
-          <div className="w-full">
+      <div className="flex flex-col h-[90vh] lg:flex-row lg:items-center lg:justify-center lg:gap-10 lg:w-[75%] lg:m-auto">
+        <div className="mt-5 relative w-3/5">
+          <div className="w-full flex flex-col justify-between items-center">
             <Image
               src={imageData[currentImg]}
-              width={700}
-              height={700}
+              width={1000}
+              height={1000}
               alt="Product"
-              className="w-[100%] object-contain"
+              className="w-[100%] object-contain lg:rounded-md"
             />
+
+            <div className="hidden gap-3 flex-row w-full mt-5 justify-between lg:flex">
+              <div>
+                <Image
+                  src="/assets/image-product-1-thumbnail.jpg"
+                  alt="Image-thumbnail"
+                  height={80}
+                  width={80}
+                  className="rounded-lg cursor-pointer active:border-[2px] border-[#ff7d1a] transition-all"
+                />
+              </div>
+              <div>
+                <Image
+                  src="/assets/image-product-2-thumbnail.jpg"
+                  alt="Image-thumbnail"
+                  height={80}
+                  width={80}
+                  className="rounded-lg cursor-pointer active:border-[2px] border-[#ff7d1a] transition-all"
+                />
+              </div>
+              <div>
+                <Image
+                  src="/assets/image-product-3-thumbnail.jpg"
+                  alt="Image-thumbnail"
+                  height={80}
+                  width={80}
+                  className="rounded-lg cursor-pointer active:border-[2px] border-[#ff7d1a] transition-all"
+                />
+              </div>
+              <div>
+                <Image
+                  src="/assets/image-product-4-thumbnail.jpg"
+                  alt="Image-thumbnail"
+                  height={80}
+                  width={80}
+                  className="rounded-lg cursor-pointer active:border-[2px] border-[#ff7d1a] transition-all"
+                />
+              </div>
+            </div>
           </div>
 
-          <div className="absolute top-[50%] flex items-center justify-between w-full px-4">
+          <div className="absolute top-[50%] flex items-center justify-between w-full px-4 lg:hidden">
             <div
               className=" bg-white rounded-[100%] flex items-center justify-center cursor-pointer"
               onClick={prevImg}
